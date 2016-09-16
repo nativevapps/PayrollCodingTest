4c797f96f18ced61278593ebe4a0bbb281446b41
Phil Heenan <pjheenan@gmail.com>
2016-09-16 16:48:26 +0100

    add initial base classes
    Create the base class for the application and associated test case. The test case will be used to run the acceptance test and validate a functioning application.
    
    It will be the responsibility of the candidate to provide an implementation of the test case that can be run to validate functionality.

---
+++ src/main/java/com/viagogo/payroll/Payroll.java
@@ -0,0 +1,4 @@
+package com.viagogo.payroll;
+
+public class Payroll {
+}
---
+++ src/test/java/com/viagogo/payroll/PayrollTest.java
@@ -0,0 +1,15 @@
+package com.viagogo.payroll;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class PayrollTest {
+
+    @Before
+    public void setup() {
+    }
+
+    @After
+    public void teardown() {
+    }
+}
