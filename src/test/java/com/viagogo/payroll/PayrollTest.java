d447c4deca00c978e46ffaa166acf42bb2adfd4a
Phil Heenan <pjheenan@gmail.com>
2016-09-16 17:49:18 +0100

    provide simple process to fetch input data
    provide methods to import the `input` data as a string ready to be validated in the exercise. Includes simple test to validate the content of the input string.

--- src/test/java/com/viagogo/payroll/PayrollTest.java
+++ src/test/java/com/viagogo/payroll/PayrollTest.java
@@ -2,8 +2,15 @@ package com.viagogo.payroll;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static com.viagogo.payroll.TestUtils.inputStreamToString;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class PayrollTest {
+
+    private static final String INPUT_FILE = readJSON("/employees.json");
 
     @Before
     public void setup() {
@@ -11,5 +18,15 @@ public class PayrollTest {
 
     @After
     public void teardown() {
+    }
+
+    @Test
+    public void testInputFile() {
+        assertNotNull(INPUT_FILE);
+        assertTrue(INPUT_FILE.contains("Mary"));
+    }
+
+    private static String readJSON(String path) {
+        return inputStreamToString(PayrollTest.class.getResourceAsStream(path));
     }
 }
---
+++ src/test/java/com/viagogo/payroll/TestUtils.java
@@ -0,0 +1,33 @@
+package com.viagogo.payroll;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+class TestUtils {
+
+    private static final String DEFAULT_CHARSET = "UTF-8";
+
+    private TestUtils() {
+        super();
+    }
+
+    static String inputStreamToString(InputStream inputStream) {
+        return inputStreamToString(inputStream, DEFAULT_CHARSET);
+    }
+
+    static String inputStreamToString(InputStream inputStream, String charset) {
+        StringBuilder fileData = new StringBuilder();
+        String currentLine;
+        try {
+            BufferedReader bufferedReader =
+                    new BufferedReader(new InputStreamReader(inputStream, charset));
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                fileData.append(currentLine).append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileData.toString();
+    }
+}
