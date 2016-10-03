7e1dcead5692dd00835087805a8aeee0053be668
Phil Heenan <pjheenan@gmail.com>
2016-10-03 15:44:48 +0100

    remove generic type not needed

--- src/main/java/com/acme/payroll/Payroll.java
+++ src/main/java/com/acme/payroll/Payroll.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Payroll {
 
     public List<Salary> getPayroll(String inputData) {
-        List<Salary> salaries = new ArrayList<Salary>();
+        List<Salary> salaries = new ArrayList<>();
 
         return salaries;
     }
