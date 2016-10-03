75c6759b6435aed12f31df7d991e3cea1c022e3a
Phil Heenan <pjheenan@gmail.com>
2016-10-03 16:52:15 +0100

    Utility method to parse json input
    moving this to the production code base

--- src/main/java/com/acme/payroll/utils/JsonUtils.java
+++ src/main/java/com/acme/payroll/utils/JsonUtils.java
@@ -1,22 +1,18 @@
-package com.acme.payroll.test;
+package com.acme.payroll.utils;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class TestUtils {
+public class JsonUtils {
 
     private static final String DEFAULT_CHARSET = "UTF-8";
-
-    private TestUtils() {
-        super();
-    }
 
     public static String inputStreamToString(InputStream inputStream) {
         return inputStreamToString(inputStream, DEFAULT_CHARSET);
     }
 
-    static String inputStreamToString(InputStream inputStream, String charset) {
+    public static String inputStreamToString(InputStream inputStream, String charset) {
         StringBuilder fileData = new StringBuilder();
         String currentLine;
         try {
